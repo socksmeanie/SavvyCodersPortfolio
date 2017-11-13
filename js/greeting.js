c3666569532b9f83ea14861cf3f771ff6e22c027
Danielle Snyder <socksmeanie@gmail.com>
2017-11-13 16:24:42 -0600

    Added Greeting and Index

---
+++ js/greeting.js
@@ -0,0 +1,11 @@
+var name = prompt('What is your name?');
+ alert('Hello ' + name);
+
+ var myCountry = prompt('Do you like the digital?');
+
+ var DigitalCheck = function(){
+   if( myCountry === 'Yes' ) alert('You are on the Digital Web!');
+   else alert('You are on the computer! You are in the vicinity of Digital!');
+ };
+
+DigitalCheck();
